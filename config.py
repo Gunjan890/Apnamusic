af803cc6a3ab8e98dc70ed937edc05048913ce68
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2026-07-02 14:25:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'xbit_kp3GFnAvdnFVDV3L6xACy-jbVBE5q5Cd') # youtube song api key
+API_KEY = getenv("API_KEY", 'AIzaSyDAbhh9mdPXbRFuihucYXEnqPhYAU4tqyg') # youtube song api key
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Gunjan890/Apnamusic",
