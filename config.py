fc9254c6da92bc54aba817892c10a3e9c85a1d2d
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2026-07-02 10:17:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002302005857))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 8353317492))
+OWNER_ID = int(getenv("OWNER_ID", 8185848457))
 
 
 # make your bots privacy from telegra.ph and put your url here 
